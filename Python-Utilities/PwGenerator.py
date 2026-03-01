2c8acebf1e36f8c186ca7f1391acb7a256c44344
Zantaril <ganfonseca@gmail.com>
2026-03-01 12:11:23 +0000

    Update PwGenerator.py

--- Python-Utilities/PwGenerator.py
+++ Python-Utilities/PwGenerator.py
@@ -1,26 +1,8 @@
-"""
-exercício 4
-
-Vamos criar um programa que gera uma palavra passe automaticamente através de X palavras 
-contidas num ficheiro de texto(ficheiro tem 2405 palavras). 
-X é 5 por defeito ou um número facultado pelo utilizador.
-
-O programa tem que abrir o ficheiro com as palavras, ler 5 palavras das 2405 aleatoriamente, 
-deve depois juntar as palavras com símbolos/números 
-entre cada uma e por fim vai imprimir a palavra passe criada na consola.
-
-"""
 import random
 import pathlib
 path = pathlib.Path(__file__).parent / 'WordsList.txt'
 file1 = open(path, 'r', encoding="utf-8")
 Lines = file1.read().splitlines()
-
-
-# Inserir cada elemento na password - DONE 
-# Inserir numeros e simbolos entre cada palavra -  DONE
-#Por cada palavra inserida na password insere
-# um simbolo ou numero
 
 
 password = []
@@ -48,12 +30,6 @@ while True:
     break
 
 print("\n \n Enjoy your password!")
-
-# TESTES TESTES
-password=""
-for i in range(escolha):
-    password += Lines[random.randrange(len(Lines))]
-    password += str(numsSymb[random.randrange(len(numsSymb))])
 
 print(password)
     
